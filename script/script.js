489cd0bc9e3d3ecb8645bde5e5acf7923acbc256
Steve Calla <callasteven@gmail.com>
2024-01-02 15:53:48 -0700

    Remove event prevent default

--- script/script.js
+++ script/script.js
@@ -4,8 +4,7 @@ let homePageButton = $("#btn-start");
 console.log(homePageButton);
 console.log('click to go to homepage');
 
-homePageButton.on("click", function (e) {
-  e.preventDefault();
+homePageButton.on("click", function () {
   // Redirect the user to the 'homepage.html' page
   location.href = "./homepage.html"
 });
