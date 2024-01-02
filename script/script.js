b876da11d42dea16c0822ccc5c27b5be06ab22b4
Steve Calla <callasteven@gmail.com>
2024-01-02 15:43:44 -0700

    Adjust homepage & dashboard path

--- script/script.js
+++ script/script.js
@@ -7,16 +7,5 @@ console.log('click to go to homepage');
 homePageButton.on("click", function (e) {
   e.preventDefault();
   // Redirect the user to the 'homepage.html' page
-  if (window.location.hostname === "yuzbamaria.github.io") {
-    // Redirect to the GitHub Pages URL
-    location.href = "./homepage.html"
-    // window.location.href =
-      // "https://yuzbamaria.github.io/job_search_tracker/homepage.html";
-      // "https://stevecalla.github.io/job_search_tracker/homepage.html";
-  } else {
-    // Redirect to the local development URL
-    location.href = "./homepage.html"
-    // window.location.href = "http://127.0.0.1:5501/job_search_tracker/homepage.html";
-    // window.location.href = "http://localhost/job_search_tracker/dashboard.html";
-  }
+  location.href = "./homepage.html"
 });
